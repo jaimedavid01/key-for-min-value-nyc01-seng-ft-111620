95255f7f5331aa6532c1e32abc9233a4bfb001bc
Jaime David <jaimedavid01@yahoo.com>
2020-10-29 03:25:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,16 +6,17 @@ require 'pry'
 
 
 def key_for_min_value(name_hash)
-  answer = nil
+  answer = nil 
   if name_hash.to_a == []
     return nil
-  else
+  else 
     num = name_hash.to_a[0][1]
   end
-  name_hash.each do |key, value|
-    if value <= num
+  name_hash.each do |name, value|
+    if num >= value
       num = value
       answer = key
+      
     end
   end
   answer
